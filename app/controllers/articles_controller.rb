fb06815c13cbf25fe840b1e20d193d486965e8e6
LaxmideviMokhi <laxmidevi.1216@gmail.com>
2020-10-24 17:05:30 +0530

    CRUD operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,37 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def create
+		#render plain: params[:article]
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		if @article.save
+			#render plain: @article.errors.full_messages
+			#redirecting to show action with id
+			#redirect_to article_path(@article)
+			#shortened article_path
+			flash[:notice] = "article was successfully created"
+			redirect_to @article #redirects to show action
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(params.require(:article).permit(:title,:description))
+			flash[:notice] = "article updated successfully"
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
 end
